a4664345273be75575f7e2a2d572c6361d8d1850
j.chavero <cmjorge4@gmail.com>
2019-05-11 17:54:48 +0200

    Amplair imagenes y fecha añadida

--- app.js
+++ app.js
@@ -37,37 +37,43 @@ function getContactPublicList(){
 
 					function (data){
 						nameMap.set(c.username , data.person.realname._content);
-						console.log("aqui tiene que funcionar");
-						console.log(nameMap.get(c.username));
 					}
 				);
 			}
 		}
-
 	);
+	console.log(nameMap.get('mikamp'));
 
 }
 
 function getContactsPhotos(){
 
+	var url= 'https://api.flickr.com/services/rest/?&method=flickr.photos.getContactsPublicPhotos&api_key='
+		+ api_key
+		+ '&user_id='
+		+ user_id
+		+ '&extras=date_taken&format=json&nojsoncallback=1';
+
 	var photoList;
 
-	$.getJSON(buildMethodURL("flickr.photos.getContactsPublicPhotos", user_id),
+	$.getJSON(url,
 
 		function(response){
 			for (var img of response.photos.photo){
-
 				username = [contactMap.get(img.username)];
 				var list = username.push(img);
 
+				console.log(img.datetaken);
 				console.log("username :" + img.username);
 				var realn = nameMap.get(img.username);
+				console.log(nameMap.get(img.username));
 				console.log("realname: " + realn);
 
 				contactMap.set(img.username, list);
-				$("#photos").append($("<img/>").attr("src",photoUrl(img)));
-				$("#photos").append("<p>" + img.username + "</p>");
-				$("#photos").append("<p>" + realn + "</p>");
+				$("#photos").append($("<img/>").attr("src",photoUrl(img)).attr("height", "150").attr("width","150").attr("onclick", "javascript:this.height=250; this.width=400").attr("ondblclick", "javascript:this.width=150;this.height=150"));
+				$("#photos").append("<p>Usuario: " + img.username + "</p>");
+				$("#photos").append("<p>Nombre: " + realn + "</p>");
+				$("#photos").append("<p>Fecha: " + img.datetaken+ "</p>");
 			}
 		}
 
@@ -81,8 +87,8 @@ function photoUrl(photo) {
 function start(){
 	showUsername();
 	getContactPublicList();
-	$.when(getContactPublicList()).then(getContactsPhotos());
-	//getContactsPhotos();
+	//$.when(getContactPublicList()).then(getContactsPhotos());
+	getContactsPhotos();
 }
 
 start()
