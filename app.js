b538bd438145cf7c8269477be5b50f6524901631
mikamp116 <mikamardanyan@gmail.com>
2019-05-12 19:43:21 +0200

    Filter added, timeline started

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ let username = "";
 
 let contactMap = new Map();
 let nameMap = new Map(); // mapa donde guardamos username y realname
+let onlyFamilyandFriends = 0; // hay que poner un interruptor para esto
+var today = new Date();
+var lastyear = today.getTime()/1000 - 31536000;
 
 
 function buildMethodURL(method, user_id){
@@ -42,39 +45,62 @@ function getContactPublicList(){
 			}
 		}
 	);
-	console.log(nameMap.get('mikamp'));
 
 }
 
 function getContactsPhotos(){
 
-	var url= 'https://api.flickr.com/services/rest/?&method=flickr.photos.getContactsPublicPhotos&api_key='
+	var url = 'https://api.flickr.com/services/rest/?&method=flickr.photos.getContactsPublicPhotos&api_key='
 		+ api_key
 		+ '&user_id='
-		+ user_id
-		+ '&extras=date_taken&format=json&nojsoncallback=1';
+		+ user_id;
+
+	if (onlyFamilyandFriends == 1 ) {
+		url += '&just_friends=1';
+	}
+
+	url += '&extras=date_taken&format=json&nojsoncallback=1';
 
 	var photoList;
 
 	$.getJSON(url,
 
-		function(response){
+		function (response){
 			for (var img of response.photos.photo){
 				username = [contactMap.get(img.username)];
 				var list = username.push(img);
 
-				console.log(img.datetaken);
-				console.log("username :" + img.username);
 				var realn = nameMap.get(img.username);
-				console.log(nameMap.get(img.username));
-				console.log("realname: " + realn);
 
 				contactMap.set(img.username, list);
-				$("#photos").append($("<img/>").attr("src",photoUrl(img)).attr("height", "150").attr("width","150").attr("onclick", "javascript:this.height=250; this.width=400").attr("ondblclick", "javascript:this.width=150;this.height=150"));
-				$("#photos").append("<p>Usuario: " + img.username + "</p>");
-				$("#photos").append("<p>Nombre: " + realn + "</p>");
-				$("#photos").append("<p>Fecha: " + img.datetaken+ "</p>");
+
+				$("#photos").append($("<img/>").attr("src", photoUrl(img)).attr("height", "150").attr("width", "150").attr("onclick", "javascript:this.height=250; this.width=400").attr("ondblclick", "javascript:this.width=150;this.height=150"));
+				$("#photos").append($("<p class='username' >Usuario: " + img.username + "</p>").attr("id", img.owner));
+				$("#photos").append("<p class='realname'>Nombre: " + realn + "</p>");
+				$("#photos").append("<p class='date'>Fecha: " + img.datetaken + "</p>");
 			}
+			$(".username").click(function() {
+				/*clickUsername($(this).attr("id"));
+				alert($(this).attr("id"));*/
+				$('#myTimeline').albeTimeline(data, {
+					//Effect of presentation
+					//'fadeInUp', 'bounceIn', etc
+					effect: 'zoomInUp',
+					//Sets the visibility of the annual grouper
+					showGroup: true,
+					//Sets the anchor menu visibility for annual groupings (depends on 'showGroup')
+					showMenu: true,
+					//Specifies the display language of texts (i18n)
+					language: 'es-ES',
+					//Sets the date display format
+					//'dd/MM/yyyy', 'dd de MMMM de yyyy HH:mm:ss', etc
+					formatDate : 'dd MMMM',
+					//Defines ordering of items
+					//true: Descendente
+					//false: Ascendente
+					sortDesc: true
+				});
+			});
 		}
 
 	);
@@ -85,12 +111,24 @@ function photoUrl(photo) {
 }
 
 function start(){
-	showUsername();
 	getContactPublicList();
-	//$.when(getContactPublicList()).then(getContactsPhotos());
+	showUsername();
 	getContactsPhotos();
 }
 
-start()
+function clickUsername(nid) {
+	let url = 'https://api.flickr.com/services/rest/?method=flickr.people.getPhotos&api_key=' + api_key +
+		'&user_id=' + nid +
+		'&min_upload_date=' + lastyear +
+		'&format=json&nojsoncallback=1';
+	$.getJSON(url,
 
+		function(response) {
+			for (var img of response.photos.photo){
+				$("#album").append($("<img/>").attr("src", photoUrl(img)));
+			}
+		}
+	);
+}
 
+start();
